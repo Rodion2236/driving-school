5d1ad9a6cd703d202e052a9b6e7d030448351fea
Rodion <kosolapkinr@gmail.com>
2023-04-14 16:50:42 +0400

    add category/categoryDetail fragments

---
+++ app/src/main/java/com/rodion2236/drivingschool/ui/CategoryFragment.kt
@@ -0,0 +1,18 @@
+package com.rodion2236.drivingschool.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rodion2236.drivingschool.R
+
+class CategoryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_category, container, false)
+    }
+}
