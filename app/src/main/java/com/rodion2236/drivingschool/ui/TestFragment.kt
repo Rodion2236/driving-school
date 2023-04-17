3d7a9de0ff5308d5d6ed6ea675724ec350a627a2
Rodion <kosolapkinr@gmail.com>
2023-04-17 18:06:57 +0400

    add anim

--- app/src/main/java/com/rodion2236/drivingschool/ui/TestFragment.kt
+++ app/src/main/java/com/rodion2236/drivingschool/ui/TestFragment.kt
@@ -1,10 +1,11 @@
-package com.rodion2236.drivingschool
+package com.rodion2236.drivingschool.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.rodion2236.drivingschool.R
 
 class TestFragment : Fragment() {
 
